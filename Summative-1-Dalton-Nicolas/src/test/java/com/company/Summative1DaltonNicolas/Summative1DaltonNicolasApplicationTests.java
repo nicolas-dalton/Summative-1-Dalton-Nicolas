288794620062221d275e0a5bbea8ea0da863a661
Nicolas Dalton <ndalton1@stedwards.edu>
2021-09-30 17:44:02 -0500

    Initial commit

---
+++ Summative-1-Dalton-Nicolas/src/test/java/com/company/Summative1DaltonNicolas/Summative1DaltonNicolasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Summative1DaltonNicolas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative1DaltonNicolasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
